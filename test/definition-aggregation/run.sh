8a921de7d720e7e93b3639278c1ba6ee11573e69
Federico Tedin <federicotedin@gmail.com>
2026-01-13 20:14:49 +0100

    Improve textDocument/definition tests

--- test/definition-aggregation/run.sh
+++ test/definition-aggregation/run.sh
@@ -9,12 +9,16 @@ FIFO=$(mktemp -u)
 mkfifo "$FIFO"
 trap "rm -f '$FIFO'" EXIT INT TERM
 
-# s1 (primary) does NOT have definitionProvider
-# s2 (secondary) has definitionProvider
-# s3 (tertiary) has definitionProvider
+# s1 does NOT have definitionProvider
+# s2 has definitionProvider, returns random locations as LocationLink (list of)
+# s3 has definitionProvider, returns random locations as Location (single, dict)
+# s4 has definitionProvider, returns const locations as LocationLink (list of)
+# s4 has definitionProvider, returns const locations as Location (single, dict)
 # Expected: definition request goes to s2 and s3, responses aggregated
-./client.py < "$FIFO" | ./../../rass \
+./client.py < "$FIFO" | python3 -m rassumfrassum \
          -- python ./server.py --name s1 \
-         -- python ./server.py --name s2 --has-definition \
-         -- python ./server.py --name s3 --has-definition --as-dict \
+         -- python ./server.py --name s2 --has-definition --as-link \
+         -- python ./server.py --name s3 --has-definition --in-dict \
+         -- python ./server.py --name s4 --has-definition --as-link --const \
+         -- python ./server.py --name s5 --has-definition --in-dict --const \
 > "$FIFO"
--- test/definition-aggregation/server.py
+++ test/definition-aggregation/server.py
@@ -4,6 +4,7 @@ Server that provides definitionProvider capability.
 """
 
 import argparse
+import random
 
 from rassumfrassum.test2 import run_toy_server
 
@@ -11,30 +12,44 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--name', required=True)
 parser.add_argument('--has-definition', action='store_true',
                    help='Whether this server provides definitions')
-parser.add_argument('--as-dict', action='store_true',
-                   help='Whether the definition provided is contained in a dictionary')
+parser.add_argument('--in-dict', action='store_true',
+                   help='Whether the definition location provided is contained in a dictionary')
+parser.add_argument('--as-link', action='store_true',
+                   help='Whether the definition location returned provided is a link')
+parser.add_argument('--const', action='store_true',
+                   help='Whether the definition location returned contains always the same range')
 args = parser.parse_args()
 
 capabilities = {}
 if args.has_definition:
     capabilities['definitionProvider'] = True
 
-definition = {
-    'uri': f'file:///{args.name}.py',
-    'range': {
-        'start': {'line': 0, 'character': 0},
-        'end': {'line': 0, 'character': 10}
+def handle_find_definition(msg_id, params):
+    startLine, startChar = 0, 0
+    endLine, endChar = 0, 10
+    if not args.const:
+        startLine, startChar = random.randint(11, 10000), random.randint(0, 1000)
+        endLine, endChar = startLine, startChar + random.randint(1, 1000)
+
+    definition = {
+        'uri': f'file:///{args.name}.py',
+        'range': {
+            'start': {'line': startLine, 'character': startChar},
+            'end': {'line': endLine, 'character': endChar}
+        }
     }
-}
 
-def build_handler(as_dict: bool):
-    def handle_definition(msg_id, params):
-        return definition if as_dict else [definition]
+    if args.as_link:
+        definition = {
+            'targetUri': definition['uri'],
+            'targetSelectionRange': definition['range'],
+            'targetRange': definition['range']
+        }
 
-    return handle_definition
+    return definition if args.in_dict else [definition]
 
 run_toy_server(
     name=args.name,
     capabilities=capabilities,
-    request_handlers={'textDocument/definition': build_handler(args.as_dict)}
+    request_handlers={'textDocument/definition': handle_find_definition}
 )
